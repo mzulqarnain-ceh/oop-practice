b0d5d87051aee825bdd0359952e721ec22e7eb36
Zulqarnai <mzulqarnainceh@gmail.com>
2026-07-13 19:45:27 +0500

    Added Doctor class and AbstractHospital class and test them

--- phms.py
+++ phms.py
@@ -32,9 +32,36 @@ class Patient(Person):
     def __str__(self):
         return f"Name: {self.name} | Patient_id: {self.patient_id} | Disease: {self.disease}"
 class Doctor(Person):
-    pass
+    def __init__(self,name,age,phone,doctor_id,specialization,patients=None):
+        super().__init__(name,age,phone)
+        self.doctor_id=doctor_id
+        self.specialization=specialization
+        self.__patients=patients or []
+    def assign_patient(self,patient):
+        self.__patients.append(patient)
+    def remove_patient(self,patient_id):
+        for patient in self.__patients:
+            if patient.patient_id==patient_id:
+                self.__patients.remove(patient)
+                print(f"{patient.name} remove ho gya ha")
+        return
+    def show_patients(self):
+        print(f"Assigned Patients are: {[patient.name for patient in self.__patients]}")
+    @staticmethod
+    def doctor_info():
+        print("Doctors available 24/7")
+    def __str__(self):
+        return f"Name: {self.name} | Doctor_id: {self.doctor_id} | Specialization: {self.specialization}"
 class AbstractHospital(ABC):
-    pass
+    @abstractmethod
+    def add_patient(self):
+        pass
+    @abstractmethod
+    def discharge_patient(self):
+        pass
+    @abstractmethod
+    def search_patient(self):
+        pass
 class Hospital(AbstractHospital):
     pass
 # p=Person("Ali",19,"0300-300")
@@ -44,3 +71,10 @@ print(pa)
 pa.medical_history="ECG normal"
 pa.add_history("CT scan Normal")
 pa.show_history()
+d=Doctor("Dr. Ali",30,"0330-4321","001","Heart surgon")
+print(d)
+d.assign_patient(pa)
+d.show_patients()
+d.doctor_info()
+d.remove_patient("001")
+# d.show_patients()
